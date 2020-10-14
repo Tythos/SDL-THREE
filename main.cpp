0175971add3a10e0e8de17d66771d145c7dd3e4f
Brian Kirkpatrick <code@tythos.net>
2020-10-14 16:57:20 -0700

    Basic rect-based blitting for sprite atlas demo

--- main.cpp
+++ main.cpp
@@ -13,8 +13,7 @@ void logSDLError(std::ostream &os, const std::string &msg) {
     os << msg << " error: " << SDL_GetError() << std::endl;
 }
 
-void cleanup(SDL_Texture* background, SDL_Texture* foreground, SDL_Renderer* renderer, SDL_Window* window) {
-    if (background) { SDL_DestroyTexture(background); }
+void cleanup(SDL_Texture* foreground, SDL_Renderer* renderer, SDL_Window* window) {
     if (foreground) { SDL_DestroyTexture(foreground); }
     if (renderer) { SDL_DestroyRenderer(renderer); }
     if (window) { SDL_DestroyWindow(window); }
@@ -28,21 +27,21 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *renderer) {
     return texture;
 }
 
-void renderTexture(SDL_Texture* tex, SDL_Renderer* ren, int x, int y) {
-    SDL_Rect dst;
-    dst.x = x;
-    dst.y = y;
-    SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-    SDL_RenderCopy(ren, tex, NULL, &dst);
+void renderTexture(SDL_Texture* tex, SDL_Renderer* ren, SDL_Rect dst, SDL_Rect* clip=nullptr) {
+    SDL_RenderCopy(ren, tex, clip, &dst);
 }
 
-void renderTexture(SDL_Texture* tex, SDL_Renderer* ren, int x, int y, int w, int h) {
+void renderTexture(SDL_Texture* tex, SDL_Renderer* ren, int x, int y, SDL_Rect* clip=nullptr) {
     SDL_Rect dst;
     dst.x = x;
     dst.y = y;
-    dst.w = w;
-    dst.h = h;
-    SDL_RenderCopy(ren, tex, NULL, &dst);
+    if (clip != nullptr) {
+        dst.w = clip->w;
+        dst.h = clip->h;
+    } else {
+        SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+    }
+    renderTexture(tex, ren, dst, clip);
 }
 
 int main(int, char**) {
@@ -68,37 +67,68 @@ int main(int, char**) {
         SDL_Quit();
         return 1;
     }
-    SDL_Texture* background = loadTexture("bin/background.png", renderer);
-    SDL_Texture* sprite = loadTexture("bin/sprite.png", renderer);
-    if (background == nullptr || sprite == nullptr) {
-        cleanup(background, sprite, renderer, window);
+    SDL_Texture* image = loadTexture("bin/image.png", renderer);
+    if (image == nullptr) {
+        cleanup(image, renderer, window);
         IMG_Quit();
         SDL_Quit();
         return 1;
     }
-
-    SDL_RenderClear(renderer);
-
-    // render background tiles
-    int xTiles = SCREEN_WIDTH / TILE_SIZE;
-    int yTiles = SCREEN_HEIGHT / TILE_SIZE;
-    for (int i = 0; i < xTiles * yTiles; ++i) {
-        int x = i % xTiles;
-        int y = i / xTiles;
-        renderTexture(background, renderer, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    int iW = 100;
+    int iH = 100;
+    int x = 0.5 * (SCREEN_WIDTH - iW);
+    int y = 0.5 * (SCREEN_HEIGHT - iH);
+    SDL_Rect clips[4];
+    for (int i = 0; i < 4; ++i) {
+        clips[i].x = i / 2 * iW;
+        clips[i].y = i % 2 * iH;
+        clips[i].w = iW;
+        clips[i].h = iH;
     }
-    int iW, iH;
+    int useClip = 0;
 
-    // render foreground sprite
-    SDL_QueryTexture(sprite, NULL, NULL, &iW, &iH);
-    int x = SCREEN_WIDTH / 2 - iW / 2;
-    int y = SCREEN_HEIGHT / 2 - iH / 2;
-    renderTexture(sprite, renderer, x, y);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(2000);
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            } else if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_1: {
+                        useClip = 0;
+                        break;
+                    }
+                    case SDLK_2: {
+                        useClip = 1;
+                        break;
+                    }
+                    case SDLK_3: {
+                        useClip = 2;
+                        break;
+                    }
+                    case SDLK_4: {
+                        useClip = 3;
+                        break;
+                    }
+                    case SDLK_ESCAPE: {
+                        quit = true;
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        renderTexture(image, renderer, x, y, &clips[useClip]);
+        SDL_RenderPresent(renderer);
+    }
 
     // close down
-    cleanup(background, sprite, renderer, window);
+    cleanup(image, renderer, window);
     IMG_Quit();
     SDL_Quit();
     return 0;
