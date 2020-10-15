2dc57983cd9b30904d6104028b43250bc78d26d8
Brian Kirkpatrick <code@tythos.net>
2020-10-14 19:51:13 -0700

    working w/ shader but uses glew (not ideal)

--- link.bat
+++ link.bat
@@ -1,3 +1,3 @@
 @echo off
 rem Links .OBJ resources and .LIB dependencies using MSVC's command-line linker to generate a binary executable
-link.exe /nologo /SUBSYSTEM:CONSOLE /libpath:"../../SDL/SDL2-2.0.12/lib/x86" /libpath:"../../SDL/SDL2_image-2.0.5/lib/x86" /libpath:"../../SDL/SDL2_ttf-2.0.15/lib/x86" /OUT:"bin/main.exe" SDL2.lib SDL2main.lib SDL2_image.lib SDL2_ttf.lib shell32.lib obj/main.obj
+link.exe /nologo /SUBSYSTEM:CONSOLE /libpath:"../../SDL/glew-2.1.0/lib/x86" /libpath:"../../SDL/SDL2-2.0.12/lib/x86" /OUT:"bin/main.exe" SDL2.lib SDL2main.lib OpenGL32.lib GLU32.lib glew32d.lib shell32.lib obj/main.obj
